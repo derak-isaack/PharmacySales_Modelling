2a2858a089996dec9bf5eb9cd7da6d3c692b339c
deerak-isaack <oderaisaack@gmail.com>
2024-06-16 00:59:37 +0300

    changed class instance to accomodate python mysql connector

--- database2.py
+++ database2.py
@@ -1,24 +1,66 @@
-import sqlite3
+import mysql.connector
 
 class Database:
-    def __init__(self, db_name='database.db'):
+    def __init__(self, db_host, db_user, db_password, db_name):
+        self.db_host = db_host
+        self.db_user = db_user
+        self.db_password = db_password
         self.db_name = db_name
-
+        self.initialize_database()
+        
     def initialize_database(self):
-        conn = sqlite3.connect(self.db_name)
+        conn = mysql.connector.connect(
+            host=self.db_host,
+            user=self.db_user,
+            password=self.db_password,
+            database = self.db_name  
+        )
         cursor = conn.cursor()
         with open('pharmacy_sales_tracker.sql', 'r') as file:
             sql_script = file.read()
-        cursor.executescript(sql_script)
-        conn.commit()
+        for statement in sql_script.split(';'):
+            if statement.strip():
+                cursor.execute(statement)
         conn.close()
+        # self.initialize_database_tables()
+
+    # def initialize_database(self):
+    #     conn = mysql.connector.connect(
+    #         host=self.db_host,
+    #         user=self.db_user,
+    #         password=self.db_password,
+    #         database=self.db_name
+    #     )
+    #     cursor = conn.cursor()
+    #     with open('pharmacy_sales_tracker.sql', 'r') as file:
+    #         sql_script = file.read()
+    #     for statement in sql_script.split(';'):
+    #         if statement.strip():
+    #             cursor.execute(statement)
+    #     conn.commit()
+    #     conn.close()
 
     def connect(self):
-        return sqlite3.connect(self.db_name)
+        return mysql.connector.connect(
+            host=self.db_host,
+            user=self.db_user,
+            password=self.db_password,
+            database=self.db_name
+        )
 
     def execute(self, sql, data):
-        conn = sqlite3.connect(self.db_name)
+        conn = mysql.connector.connect(
+            host=self.db_host,
+            user=self.db_user,
+            password=self.db_password,
+            database=self.db_name
+        )
         cursor = conn.cursor()
         cursor.execute(sql, data)
         conn.commit()
         conn.close()
+        
+host = 'localhost'
+user = 'root'
+password = '@admin#2024*10'
+database = 'pharmaflow'
