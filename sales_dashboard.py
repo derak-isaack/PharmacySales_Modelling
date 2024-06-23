4041d598a0dc141258eadc66f568c4a3150f8ba1
deerak-isaack <oderaisaack@gmail.com>
2024-06-23 13:01:06 +0300

    modified quixstreams code to publish data to kafka

--- sales_dashboard.py
+++ sales_dashboard.py
@@ -5,6 +5,13 @@ from helpers import App
 import mysql.connector
 import socket
 import json
+from quixstreams import Application
+
+#Configure streamlit with quixstreams
+app = Application(
+    broker_address= 'localhost:9092',
+    loglevel= 'DEBUG'
+)
 
 db = Database(db_user='root', db_password='@admin#2024*10', db_host='localhost', db_name='pharmaflow')
 db.initialize_database()  
@@ -34,26 +41,38 @@ def get_doctor_names():
     conn.close()
     return names   
 
-def send_data(data):
-    """Sends individual data points from `data` to ncat server.
+# def send_data(data):
+#     """Sends individual data points from `data` to ncat server.
 
-    Args:
-        data (str): The data string containing comma-separated values.
-    """
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("localhost", 8000))  
+#     Args:
+#         data (str): The data string containing comma-separated values.
+#     """
+#     try:
+#         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         s.connect(("localhost", 8000))  
 
-        # Split data into individual values 
-        data_points = data.split(",")
-        for value in data_points:
-            s.sendall(f"{value}\n".encode('utf-8'))
+#         # Split data into individual values 
+#         data_points = data.split(",")
+#         for value in data_points:
+#             s.sendall(f"{value}\n".encode('utf-8'))
             
-        s.close()
+#         s.close()
         
-    except ConnectionError as e:
-        st.error(f"Error sending data to ncat server: {e}")
+#     except ConnectionError as e:
+#         st.error(f"Error sending data to ncat server: {e}")
 
+# def send_data_quixstreams(data):
+#     app = Application(
+#         broker_address="localhost:9092",
+#         loglevel="DEBUG"
+#     )
+#     with app.get_producer() as producer:
+#         producer.produce(
+#             topic="pharmacy-sales-stream",
+#             value=json.dumps(data)
+#         )
+        
+        
 def main():
 
     st.set_page_config(page_title='Sales Dashboard', page_icon=':bar_chart:', layout='wide')
@@ -90,7 +109,12 @@ def main():
             "date_sold": str(date_sold)
         }
         data_json = json.dumps(data_dict)
-        send_data(data_json)
+        with app.get_producer() as producer:
+            producer.produce(
+                topic="pharmacy-sales-stream",
+                value=json.dumps(data_json)
+            )
+        # send_data_quixstreams(data_json)
         st.success('Data sent to the server successfully!')
 
 
